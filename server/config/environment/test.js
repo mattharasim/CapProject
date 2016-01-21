124200e312fede2b40b3c52636092a1ec57a08b0
mattharasim <Matt@Matts-iMac.local>
2016-01-20 19:15:22 -0600

    added mongo uri

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/capproject-test'
+    uri: 'mongodb://heroku_ffj3n01b:bn21vecqnp78k78ru1f4n5qjg2@ds047315.mongolab.com:47315/heroku_ffj3n01b'
   },
   sequelize: {
     uri: 'sqlite://',
